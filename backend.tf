c0358a78f4ae99b2572d4bd7e575b8322d5b37f2
LedouxS <99309845+LedouxS@users.noreply.github.com>
2022-08-04 18:59:31 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-file-tshl"
     key    = "ledoux/s3file-3.tfstate"
-    region = "us-west-2"
+    region = "us-west-1"
     profile = "default"
 dynamodb = "terraformtable"
   }
