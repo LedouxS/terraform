d6a1d8c259877b11d54929caa2744ef2152c0d32
LedouxS <99309845+LedouxS@users.noreply.github.com>
2022-08-04 19:07:46 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-file-tshl"
-    key    = "ledoux/s3file-3.tfstate"
-    region = "us-west-1"
-    profile = "default"
-dynamodb = "terraformtable"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-state-file-tshl"
+#     key    = "ledoux/s3file-3.tfstate"
+#     region = "us-west-1"
+#     profile = "default"
+# dynamodb = "terraformtable"
+#   }
+# }
 
